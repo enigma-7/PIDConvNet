adc3e6c81e4e2feea744b4d5b77b55470ca36745
Jibran Bohra <aabohra17@gmail.com>
2019-07-17 21:04:22 +0200

    Split.

--- probe_data.py
+++ probe_data.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 import random, matplotlib
-from TOOLS import DATA, PLOT
+from TOOLS import DATA
 matplotlib.rcParams.update({'font.size': 16})
 matplotlib.rcParams['text.usetex'] = True
 
@@ -12,17 +14,45 @@ raw_data = np.load(directory + '0_tracks.npy')
 raw_info = np.load(directory + '0_info_set.npy')
 print("Loaded: %s" % directory)
 
+plotdir = '/home/jibran/Desktop/neuralnet/plots/'
 dataset, infoset = DATA.process_1(raw_data, raw_info)
+columns = ["label", "nsigmae", "nsigmap", "PT", "${dE}/{dx}$", "Momenta [GeV]", "eta", "theta", "phi", "event", "V0trackID",  "track"]
+infoframe = pd.DataFrame(data=infoset, columns=columns)
+infoframe.head()
+#infoframe["ADCsum"] = dataset.sum(axis=(1,2,3))
+#infoframe["label"] = ["$\\pi$" if l == 0 else "$e^-$" for l in infoset[:,0]]
+#g = sns.pairplot(data=infoframe[["label", "dEdX", "P", "ADCsum"]], hue="label", diag_kind='hist', palette={"$\\pi$":'r',"$e^-$":'g'})
+#g.map_diag(plt.hist, log=True, edgecolor='black')
+
 
 elec_data, elec_info = DATA.elec_strip_(dataset, infoset)
 pion_data, pion_info = DATA.pion_strip_(dataset, infoset)
 
 class_data = [pion_data, elec_data]
 class_info = [pion_info, elec_info]
-cnames = ["$\\pi$","$e^-$"]
+cnames = ["$\\pi$","$e$"]
 colour = ['r', 'g']
 
-fig, axes = plt.subplots(1, 3, figsize=(17,5))
+fig, axes = plt.subplots(1, 3, figsize=(15,5))
+for j,n in enumerate([4,5]):
+    for i in range(2):
+        x = class_info[i][:,n]
+        if i>0:
+            axes[j].hist(x, edgecolor='black', color = colour[i],
+                bins=bins[bins< np.ceil(x.max())], alpha=0.6, label=cnames[i])
+            #axes[0].set_xticks([round(j,2) for j in bins[bins< np.ceil(x.max())]][::1])
+        else:
+            counts, bins, patches = axes[j].hist(x, edgecolor='black',
+                color = colour[i], bins=14, alpha=0.6, label=cnames[i])
+        y = x[(x>bins[0]) & (x<bins[1])]
+        axes[j].set_xticks([round(j,2) for j in bins][::3])
+        axes[j].set_xlabel(columns[n])
+        axes[j].set_ylabel("Counts")
+        axes[j].set_yscale('log')
+        axes[j].legend()
+
+
+fig, axes = plt.subplots(1, 3, figsize=(15,5))
 for i in range(2):
     x = class_info[i][:,5]
     if i>0:
@@ -44,7 +74,7 @@ axes[0].legend()
 axes[1].set_xticks([round(i,2) for i in b0][::2])
 axes[1].set_xlabel("Momenta [GeV]")
 axes[1].set_ylabel("Counts")
-axes[1].set_yscale('log')
+#axes[1].set_yscale('log')
 axes[1].legend()
 
 elec_momenta = elec_info[:,5]
@@ -59,6 +89,7 @@ axes[2].set_xlabel("Momenta [GeV]")
 axes[2].grid()
 axes[2].set_xticks([round(i,1) for i in bins_e[bins_e < round(elec_momenta.max(),1)]][::5])
 
+plt.savefig(plotdir + 'momenta_hist.png')
 plt.show()
 
 fig, axes = plt.subplots(1, 3, figsize=(17,5))
@@ -79,7 +110,17 @@ axes[2].set_xlabel("Time bin no.")
 axes[2].set_ylabel("Mean ADC value per pad")
 axes[2].grid()
 
-"""
+#fig, axes = plt.subplots(1, 2, figsize=(12,5))
+plt.figure(figsize=(8,6))
+ADCsum = [x.sum(axis=(1,2,3)) for x in class_data]
+plt.boxplot(ADCsum, labels=cnames)
+plt.grid()
+plt.yscale('log')
+plt.ylabel("ADC sum")
+plt.title("")
+plt.show()
+class_data[1][(ADCsum[1]<10.0)].shape
+
 fig, axes = plt.subplots(1, 2, figsize=(12,5))
 for i,c in enumerate(class_data):
     Z = np.sum(c, axis=(0,3))/c.shape[0]
@@ -96,4 +137,3 @@ for i,c in enumerate(class_data):
     #fig.colorbar(im)#, cax=cbar_a)
 
 #fig.title('Mean tracklet per class')
-"""
---
+++ probe_model.py
@@ -0,0 +1,59 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import random, matplotlib
+from TOOLS import PLOT, METRICS, DATA
+matplotlib.rcParams.update({'font.size': 16})
+matplotlib.rcParams['text.usetex'] = True
+
+from keras.models import load_model
+from keras.utils import CustomObjectScope
+from keras.initializers import glorot_uniform
+from scipy.signal import convolve2d
+
+run_no = '000265378/'
+dataname = 'test/'
+directory = 'data/output/' + run_no + dataname
+raw_data = np.load(directory + '0_tracks.npy')
+raw_info = np.load(directory + '0_info_set.npy')
+print("Loaded: %s" % directory)
+
+sumtracks = [np.sum(raw_info[:,6] == i) for i in range(7)]               #Sanity check
+dataset, infoset = DATA.process_1(raw_data, raw_info)
+
+X, y = DATA.shuffle_(dataset/1024, infoset[:,0])
+plt.imshow(X[0][:,:,0])
+
+modldir = "saved_models/"
+conv_sizes1 = [16]
+conv_sizes2 = [64]
+dense_sizes1 = [1024]
+dense_sizes2 = [256]
+
+for conv_size1 in conv_sizes1:
+    for conv_size2 in conv_sizes2:
+        for dense_size1 in dense_sizes1:
+            for dense_size2 in dense_sizes2:
+                mname = "conv-%d-%d-filters-dense-%d-%d-nodes-"%(conv_size1,
+                    conv_size2, dense_size1, dense_size2)
+                with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
+                    model = load_model(modldir + mname + ".h5", custom_objects={'F1': METRICS.F1})
+
+conv_1 = model.get_weights()[0]
+num = random.randint(1,dataset.shape[0])
+tracklet = dataset[num,:,:,0]
+trackids = infoset[num][-3:].astype(int)
+
+plt.imshow(tracklet)
+plt.title("Displaying tracklet %s:"%"/".join([str(i) for i in trackids]))
+output = np.array([convolve2d(tracklet, conv_1[:,:,0,i], mode='same') for i in range(16)])
+output = output.swapaxes(0,1).swapaxes(1,2)
+output = output.reshape(output.shape[0],output.shape[1],1,output.shape[2])
+
+shapearr =  [model.get_weights()[i].shape for i in range(len(model.get_weights()))]
+shapearr
+#PLOT.tileplot_(conv_1)
+#PLOT.tileplot_(output, title="Convolved tracklets %s:"%"/".join([str(i) for i in trackids]))
+
+model
